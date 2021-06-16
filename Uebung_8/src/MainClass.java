f7560182ea1f939f3cb6a73d48b38b7711c58e58
hauke <hauke@hauke-easynotets11hr>
2021-06-16 23:30:09 +0200

    Bisheriger Fortschritt für die Road Klasse. Klappt noch nicht so ganz. Insbesondere die Längen Berechnung

--- Uebung_8/src/MainClass.java
+++ Uebung_8/src/MainClass.java
@@ -6,5 +6,23 @@ public class MainClass {
 		aQuarter.addItem(new Tree(144, 261));
 		
 		SimpleIO.println("Preis des Stadtteils: " + aQuarter.getPrice());
+		
+		
+		Road aRoad = new Road(50, 100);
+		
+		aRoad.addPoint(110, 180);
+		aRoad.addPoint(250, 150);
+		aRoad.addPoint(320, 220);
+		aRoad.addPoint(400, 220);
+		
+		aRoad.print();
+		
+		aRoad.getLength();
+		
+		
+		
+		
+		
+		
 	}
 }
